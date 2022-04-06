d7cb0c8324e0178392f2fa6b71e9029b85785c13
Andrew-Sem <avvyayvayva@gmail.com>
2022-04-06 19:28:54 +0300

    init commit

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+import {privateRoutes, publicRoutes} from "../routes";
+
+const AppRouter = () => {
+  const auth = false;
+  return (
+    auth ?
+      <Routes>
+        {privateRoutes.map(({ path, element }) => (
+          <Route path={path} element={element} key={path} />
+        ))}
+      </Routes>
+      :
+      <Routes>
+        {publicRoutes.map(({ path, element }) => (
+          <Route path={path} element={element} key={path} />
+        ))}
+      </Routes>
+  );
+};
+
+export default AppRouter;
